3c1959e36c9e810fcf772bbbbd23ce206a337e00
maximecroteau <maxime.croteau85@gmail.com>
2019-12-13 15:28:49 +0100

    link pages

--- aomae/views.py
+++ aomae/views.py
@@ -6,3 +6,19 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'index.html')
+
+
+def shop(request):
+
+    return render(request, 'shop.html')
+
+
+def contact(request):
+
+    return render(request, 'contact.html')
+
+
+def cart(request):
+
+    return render(request, 'cart.html')
+
